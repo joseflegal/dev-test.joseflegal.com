d7d0c0b0b29fbfb7ce72d91cadb1d0d088aee42f
dpham06 <dpha0005@student.monash.edu>
2021-06-03 20:08:00 +1000

    Update Dashboard view, Add ImageCard component

---
+++ src/components/ImageCard.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="card" :style="cssProps">
+    <slot name="card-img"></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "src/styles/resources/_variables.scss";
+
+.card {
+  position: relative;
+
+  width: var(--width);
+  min-width: var(--min-width);
+  max-width: var(--max-width);
+
+  height: auto;
+  min-height: var(--min-height);
+  max-height: var(--max-height);
+
+  &::after {
+    content: "";
+    display: block;
+    padding-bottom: 100%;
+  }
+}
+</style>
+
+<script>
+export default {
+  props: {
+    /**
+     * The width percentage size the card will try to expand to.
+     */
+    width: {
+      type: Number,
+      default: 25,
+    },
+    /**
+     * The minimum width (px) of the card.
+     */
+    minWidth: {
+      type: Number,
+      default: 300,
+    },
+    /**
+     * The maximum width (px) of the card.
+     */
+    maxWidth: {
+      type: Number,
+      default: 500,
+    },
+    /**
+     * The minimum height (px) of the card.
+     */
+    minHeight: {
+      type: Number,
+      default: 300,
+    },
+    /**
+     * The maximum height (px) of the card.
+     */
+    maxHeight: {
+      type: Number,
+      default: 500,
+    },
+  },
+  computed: {
+    /**
+     * Converting the component's properties to CSS, for use in CSS.
+     */
+    cssProps() {
+      return {
+        "--width": this.width + "%",
+        "--min-width": this.minWidth + "px",
+        "--max-width": this.maxWidth + "px",
+        "--min-height": this.minHeight + "px",
+        "--max-height": this.maxHeight + "px",
+      };
+    },
+  },
+};
+</script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,29 +1,90 @@
 <template>
   <div class="home">
     <h1>Josef coding task</h1>
-    <!-- Render the files data with a resuable component (a list or card up to you!) -->
-    <pre>// TODO: replace this block 👇 with a resusable component that renders elements from the files array 
-      <code>files:
-      {{files}}</code>
-    </pre>
+    <h2>Rendering the list of files from the API with a reusable component:</h2>
+    <div class="card-wrapper">
+      <ImageCard v-for="file in files" :key="file.id" class="card" :width="24">
+        <img
+          slot="card-img"
+          class="card-img"
+          :src="file.src"
+          :alt="file.description"
+        />
+      </ImageCard>
+    </div>
+    <h2>
+      Create a computed property that filters the files for tags including
+      'kitten' then sorts them by date:
+    </h2>
+    <div class="cat-card-wrapper">
+      <ImageCard
+        v-for="file in kittenFiles"
+        :key="file.id"
+        :width="32"
+        class="cat-card"
+      >
+        <img
+          slot="card-img"
+          class="cat-card-img"
+          :src="file.src"
+          :alt="file.description"
+        />
+      </ImageCard>
+    </div>
   </div>
 </template>
+
 <style lang="scss" scoped>
+@import "src/styles/resources/_variables.scss";
+
 .home {
- }
- pre,code{
-  overflow-x: auto;
-  white-space: pre-wrap;
-  white-space: pre-wrap;
-  line-break: anywhere;
+  padding-left: $page-padding;
+  padding-right: $page-padding;
+
+  .card-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+
+    .card {
+      margin-right: $small-spacing;
+      margin-bottom: $small-spacing;
+
+      .card-img {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        border-radius: $base-border-radius;
+        object-fit: cover;
+      }
+    }
+  }
+
+  .cat-card-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+
+    .cat-card {
+      margin-right: $small-spacing;
+      margin-bottom: $small-spacing;
+
+      .cat-card-img {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+  }
 }
 </style>
 
 <script>
 // @ is an alias to /src
 import api from "@/api";
+import ImageCard from "@/components/ImageCard";
 
 export default {
+  components: { ImageCard },
   name: "Dashboard",
   data() {
     return {
@@ -35,6 +96,28 @@ export default {
       console.log(res);
       this.files = res;
     });
+  },
+  computed: {
+    /**
+     * Filters 'files' data for tags including 'kitten', then sort by date.
+     */
+    kittenFiles() {
+      return this.files
+        .reduce((result, file) => {
+          // Convert entire 'tags' string to lowercase in event of uppercase characters in tags.
+          if (file.tags.toLowerCase().includes("kitten")) {
+            result.push(file);
+          }
+          return result;
+        }, [])
+        .sort((a, b) => {
+          if (a.date < b.date) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
+    },
   },
 };
 </script>
