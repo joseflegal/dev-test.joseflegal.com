4c3d17c37a498749ebae1036392413fa67964e11
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2021-12-08 18:38:06 -0600

    Included Bootsrap in the project

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import router from "./router";
 import store from "./store";
 import "@/components/globals/globals";
 import "@/styles/admin.scss";
+import { BootstrapVue } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
