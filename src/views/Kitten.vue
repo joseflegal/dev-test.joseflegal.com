bd24a41a4a35242320f2a7d96eae26e415fe4462
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2021-12-08 18:38:23 -0600

    Created Kitten component

---
+++ src/views/Kitten.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="kitten-box-container">
+    <b-jumbotron header="Kittens">
+      <div>
+        <b-jumbotron>
+          <template #lead>
+            <ul>
+              <li>Description: {{ nameOfFile.description }}</li>
+              <li>Filename: {{ nameOfFile.filename }}</li>
+              <li>Mimetype: {{ nameOfFile.mimetype }}</li>
+              <li>Tags: {{ nameOfFile.tags }}</li>
+              <li>Date: {{ nameOfFile.date }}</li>
+              <li>SRC: {{ nameOfFile.src }}</li>
+            </ul>
+          </template>
+
+          <hr class="my-4" />
+
+          <b-button @click="previous" variant="primary" href="#"
+            >Previous</b-button
+          >
+          <b-button @click="next" variant="success" href="#">Next</b-button>
+        </b-jumbotron>
+      </div>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    nameOfFile: Object,
+    next: Function,
+    previous: Function,
+  },
+};
+</script>
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -3,17 +3,23 @@
     <h1>Vue.js layout</h1>
     <!-- Render the files data with a resuable component (a list or card up to you!) -->
     <pre>// TODO: replace this block 👇 with a resusable component that renders elements from the files array 
-      <code>files:
-      {{files}}</code>
+      <code>
+        <Kitten 
+        :nameOfFile="files[index]" 
+        :next="next"
+        :previous="previous"
+        />
+  
+      </code>
     </pre>
   </div>
 </template>
+
 <style lang="scss" scoped>
 .container {
   max-width: 1024px;
   margin: 0 auto;
 }
-
 pre,
 code {
   overflow-x: auto;
@@ -22,16 +28,31 @@ code {
   line-break: anywhere;
 }
 </style>
+
 <script>
 // @ is an alias to /src
 import api from "@/api";
+import Kitten from "./Kitten.vue";
 
 export default {
   name: "Layout",
+  components: {
+    Kitten,
+  },
+
   data() {
     return {
       files: [],
+      index: 0,
     };
+  },
+  methods: {
+    next() {
+      this.index++;
+    },
+    previous() {
+      this.index--;
+    },
   },
   created() {
     api.files.get().then((res) => {
