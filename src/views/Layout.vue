64abfb157bc07520f0f70b0c8b646686fe9e94bc
webdev522 <webdev522@gmail.com>
2022-04-05 02:38:15 +0900

    render files in table

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="container">
+  <div class="layout">
     <h1>Vue.js layout</h1>
+    <Table :files="files" :tag="tag" />
     <!-- Render the files data with a resuable component (a list or card up to you!) -->
-    <pre>// TODO: replace this block 👇 with a resusable component that renders elements from the files array 
+    <!-- <pre>// TODO: replace this block 👇 with a resusable component that renders elements from the files array 
       <code>files:
       {{files}}</code>
-    </pre>
+    </pre> -->
   </div>
 </template>
 <style lang="scss" scoped>
-.container {
+.layout {
   max-width: 1024px;
   margin: 0 auto;
 }
-
 pre,
 code {
   overflow-x: auto;
@@ -24,19 +24,25 @@ code {
 </style>
 <script>
 // @ is an alias to /src
-import api from "@/api";
-
+// import api from "@/api";
+import Table from "@/components/Table";
 export default {
   name: "Layout",
+  components: {
+    Table,
+  },
   data() {
     return {
-      files: [],
+      tag: "kitten",
     };
   },
+  computed: {
+    files() {
+      return this.$store.state.file.files;
+    },
+  },
   created() {
-    api.files.get().then((res) => {
-      this.files = res;
-    });
+    this.$store.dispatch("file/getAll");
   },
 };
 </script>
