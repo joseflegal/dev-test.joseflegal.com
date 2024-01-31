78833dc13a7b91e5fd2691a29bd4e1beb3043b9c
Abdo-461 <abdulrahmanmvp93@gmail.com>
2024-01-31 08:58:30 +0800

    Front-End Task

---
+++ src/views/ItemList.vue
@@ -0,0 +1,29 @@
+<!-- This is a reusable component that renders items from a predfined list/file -->
+<template>
+    <div>
+      <div v-for="item in items" :key="item.id">
+        <!--slot to render items -->
+        <slot :item="item">
+          <div>
+            <h3>{{ item.name }}</h3>
+            <p>Date: {{ item.date }}</p>
+            <ul>
+              <li v-for="tag in item.tags" :key="tag">{{ tag }}</li>
+            </ul>
+          </div>
+        </slot>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      items: {
+        type: Array,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
