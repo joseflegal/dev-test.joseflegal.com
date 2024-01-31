052a99d3f673160e9b54855f04c54e3a8cdb7947
Jordan Mathew <jordanmathew@Jordans-MacBook-Pro.local>
2024-02-01 10:14:59 +1030

    Frontend Task

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="container">
-    <h1>Vue.js layout</h1>
-    <!-- Render the files data with a resuable component (a list or card up to you!) -->
-    <pre>// TODO: replace this block 👇 with a resusable component that renders elements from the files array 
-      <code>files:
-      {{files}}</code>
-    </pre>
+  <div>
+    <api-data :files="filterImagesByKittensAndSortItByDate">
+      <template v-slot:item="{ file }">
+        <div>
+          <strong>{{ file.description }}</strong>
+          <p></p>
+          <img :src = "file.src" />
+        </div>
+      </template>
+    </api-data>
   </div>
 </template>
+
+
 <style lang="scss" scoped>
 .container {
   max-width: 1024px;
@@ -25,18 +30,34 @@ code {
 <script>
 // @ is an alias to /src
 import api from "@/api";
+import ApiData from "@/components/ApiData.vue"
 
 export default {
-  name: "Layout",
+  name: "List",
   data() {
     return {
       files: [],
+      filteredData: [],
     };
   },
   created() {
     api.files.get().then((res) => {
       this.files = res;
     });
+  },
+  components: {
+    ApiData,
+  },
+
+  computed: {
+    filterImagesByKittensAndSortItByDate() {
+      // Filter images by type 'kitten'
+      const kittenFiles = this.files.filter(file => file.tags.includes('kitten'));
+      // Sort images by date in ascending order
+      const sortedFiles = kittenFiles.sort((elementOne, elementTwo) => new Date(elementOne.date) - new Date(elementTwo.date));
+      
+      return sortedFiles;
+    },
   },
 };
 </script>
