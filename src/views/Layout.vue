537cbc7e3bd901f671b38c2e66c960323447785c
Fai Wong <wongwf82@gmail.com>
2024-02-01 16:27:59 +0800

    Filters it for tags that include kitten and sorts by date

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="container">
     <h1>Vue.js layout</h1>
-    <JoList :items="files" />
+    <JoList :items="sortedKittensFiles" />
   </div>
 </template>
+
 <style lang="scss" scoped>
 .container {
   max-width: 1024px;
   margin: 0 auto;
 }
 </style>
+
 <script>
 import api from "@/api";
 
@@ -19,6 +21,19 @@ export default {
     return {
       files: [],
     };
+  },
+  computed: {
+    kittensFiles() {
+      return this.files.filter(
+        (file) => file.tags && file.tags.includes("kitten")
+      );
+    },
+    sortedKittensFiles() {
+      const kittensFilesCopy = [...this.kittensFiles];
+      return kittensFilesCopy.sort(
+        (a, b) => new Date(b.date) - new Date(a.date)
+      );
+    },
   },
   created() {
     api.files.get().then((res) => {
