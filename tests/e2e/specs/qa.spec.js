09a099ef8cc906b1650e7b5dc74b6d44a20cf5ad
Zane Keeley <zanekeeley@gmail.com>
2024-02-06 11:04:36 +1000

    removing delays and add unhappy path to qa test

--- tests/e2e/specs/qa.spec.js
+++ tests/e2e/specs/qa.spec.js
@@ -1,26 +1,39 @@
 describe("Test QA Component", () => {
+
+  // happy path test
   it('when submitting a form with correct details - it displays the new record', () => {
     cy.visit("http://localhost:9002/#/qa");
 
-    cy.wait(2000);
-    cy.get('[data-cy="name-input"]').type('Cody', { delay: 300 });
+    cy.get('[data-cy="name-input"]').type('Cody');
 
-    cy.wait(2000);
-    cy.get('[data-cy="email-input"]').type('cody@gmail.com', { delay: 300 });
+    cy.get('[data-cy="email-input"]').type('cody@gmail.com');
 
-    cy.wait(2000);
-    cy.get('[data-cy="age-input"]').type('25', { delay: 300 });
+    cy.get('[data-cy="age-input"]').type('25');
 
-    cy.wait(2000);
-    cy.get('[data-cy="phone-input"]').type('0411112222', { delay: 300 });
+    cy.get('[data-cy="phone-input"]').type('0411112222');
 
-    cy.wait(2000);
     cy.get('[data-cy="cy-button"]').click();
 
-    cy.wait(2000);
     cy.get('[data-cy="form-record"]').contains('Cody');
     cy.get('[data-cy="form-record"]').contains('cody@gmail.com');
     cy.get('[data-cy="form-record"]').contains('25');
     cy.get('[data-cy="form-record"]').contains('0411112222');
+  });
+
+  // unhappy path end to end test
+  it('when submitting a form with with no name input - it does not submit the form', () => {
+    cy.visit("http://localhost:9002/#/qa");
+
+    cy.get('[data-cy="email-input"]').type('bob@gmail.com');
+
+    cy.get('[data-cy="age-input"]').type('10');
+
+    cy.get('[data-cy="phone-input"]').type('0422221111');
+
+    cy.get('[data-cy="cy-button"]').click();
+
+    // check form output and submit button
+    cy.get('[data-cy="cy-button"]').should('exist');
+    cy.get('[data-cy="form-record"]').should('not.exist');
   });
 });
