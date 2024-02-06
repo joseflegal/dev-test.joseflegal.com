fcd20f7f621de8984785c46cd99036db1712cb35
Nourena Elgamal <nourenanabil751@gmail.com>
2024-02-06 14:52:59 +0200

    Added some style to FileItem component to give the div a card look

--- src/components/fileComponents/FileItem.vue
+++ src/components/fileComponents/FileItem.vue
@@ -14,3 +14,15 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.file-item {
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+  width: 300px;
+  padding: 20px;
+  margin: 20px;
+}
+</style>
