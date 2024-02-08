6a266a642a8ae986385c80ba111c4ff21e7f0e6e
Nourena Elgamal <nourenanabil751@gmail.com>
2024-02-08 21:15:39 +0200

    Added unit tests in case of only 1 rule exist in checkGroup method

--- tests/unit/rules.spec.js
+++ tests/unit/rules.spec.js
@@ -1,4 +1,3 @@
-// Import Vue testing utilities and the component to test
 import { shallowMount } from "@vue/test-utils";
 import Rules from "@/views/Rules.vue";
 
@@ -12,6 +11,16 @@ describe("Rules.vue", () => {
     2: {
       logic: "any",
       rule_ids: [2, 3],
+      rule_group_ids: [],
+    },
+    3: {
+      logic: "all",
+      rule_ids: [1],
+      rule_group_ids: [],
+    },
+    4: {
+      logic: "any",
+      rule_ids: [1],
       rule_group_ids: [],
     },
   };
@@ -112,6 +121,40 @@ describe("Rules.vue", () => {
 
     // Access the checkGroup method from the component instance
     const checkGroupResult = wrapper.vm.checkGroup(ruleGroups[2]);
+
+    // Expect the result to be false as per the provided constant data
+    expect(checkGroupResult).toBe(false);
+  });
+  it('Only one rule exist and no groups, With logic === "any"', () => {
+    // Mock the created hook to assign constant data to component's data properties
+    Rules.created = jest.fn().mockImplementation(function () {
+      this.answers = answers;
+      this.rules = rules;
+      this.rule_groups = ruleGroups;
+    });
+
+    // Mount the component
+    const wrapper = shallowMount(Rules);
+
+    // Access the checkGroup method from the component instance
+    const checkGroupResult = wrapper.vm.checkGroup(ruleGroups[4]);
+
+    // Expect the result to be true as per the provided constant data
+    expect(checkGroupResult).toBe(true);
+  });
+  it('Not All rules and groups apply, With logic === "any"', () => {
+    // Mock the created hook to assign constant data to component's data properties
+    Rules.created = jest.fn().mockImplementation(function () {
+      this.answers = wrongAnswersScenario;
+      this.rules = rules;
+      this.rule_groups = ruleGroups;
+    });
+
+    // Mount the component
+    const wrapper = shallowMount(Rules);
+
+    // Access the checkGroup method from the component instance
+    const checkGroupResult = wrapper.vm.checkGroup(ruleGroups[4]);
 
     // Expect the result to be false as per the provided constant data
     expect(checkGroupResult).toBe(false);
